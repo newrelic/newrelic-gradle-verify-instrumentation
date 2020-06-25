0f0821ac0061335f74fb8aee7675f1d0c7d7ac79
Xi Xia <xxia@newrelic.com>
2020-06-24 20:49:00 -0700

    hacking at signing artifacts

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     id("groovy")
     id("java-gradle-plugin")
     id("maven-publish")
+    id("signing")
 }
 
 java {
@@ -54,7 +55,6 @@ gradlePlugin {
     }
 }
 
-
 publishing {
     repositories {
         maven {
@@ -72,4 +72,11 @@ publishing {
             from(components["java"])
         }
     }
+}
+
+signing {
+//    val signingKey: String? by project
+//    val signingPassword: String? by project
+//    useInMemoryPgpKeys(signingKey, signingPassword)
+    sign(publishing.publications["mavenJava"])
 }
