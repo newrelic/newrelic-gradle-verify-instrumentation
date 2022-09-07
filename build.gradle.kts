af3659b7e4ea46a48e9dce92b91c116511ddbf08
André Onuki <otanuki@gmail.com>
2022-09-07 12:06:37 -0400

    Updating next development version

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,7 +40,7 @@ group = "com.newrelic.agent.java"
 // -Prelease=true will render a non-snapshot version
 // All other values (including unset) will render a snapshot version.
 val release: String? by project
-version = "4.0" + if("true" == release) "" else "-SNAPSHOT"
+version = "4.1" + if("true" == release) "" else "-SNAPSHOT"
 
 tasks.jar {
     from ("LICENSE")
